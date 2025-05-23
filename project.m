4f45ff583f36609f6e422746fc2629da7be5bf44
Sam Cantwell <sam@samcantwell.au>
2025-05-23 17:13:34 +1000

    Added comments

--- project.m
+++ project.m
@@ -1,14 +1,22 @@
+% Algorithm hyperparameters
+% These were decided by manual testing aiming to reduce the time taken to
+% run the algorithms
 k = 100;
 tolerance1 = 0.1;
 tolerance2 = 0.01;
 t = 0.1;
 
+% Rotates a given vector. Used to create clean graphs when extending.
 function v = rotate(v, theta)
     R = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
     v = R*v;
 end
 
+% Extends a given graph by turing a randomly chosen terminal point into a
+% Steiner point, and then adding two new terminals connected to that
+% Steiner.
 function T = extend(T)
+   % Randomly choose a terminal
    h = height(T.Nodes);
    index = randi(h);
    while T.Nodes.Steiner{index,1} == 1
@@ -16,6 +24,7 @@ function T = extend(T)
    end
    T.Nodes.Steiner(index) = {1};
 
+   % Generate the locations of the two new points
    neighbor = neighbors(T, index);
    s_1 = [T.Nodes.x{index,1}; T.Nodes.y{index,1}];
    s_2 = [T.Nodes.x{neighbor,1}; T.Nodes.y{neighbor,1}];
@@ -27,7 +36,7 @@ function T = extend(T)
    x_2 = v_2(1) / norm(v_2) + s_1(1) + (rand() - 0.5) * 0.3;
    y_2 = v_2(2) / norm(v_2) + s_1(2) + (rand() - 0.5) * 0.3;
 
-   % Must properly calculate locations
+   % Add these new points to the graph table
    NodeProperties_1 = table({x_1}, {y_1}, {0}, VariableNames={'x', 'y', 'Steiner'});
    T = addnode(T, NodeProperties_1);
    NodeProperties_2 = table({x_2}, {y_2}, {0}, VariableNames={'x', 'y', 'Steiner'});
@@ -37,6 +46,8 @@ function T = extend(T)
    T = addedge(T, [index index], [h+1 h+2], [0 0]);
 end
 
+% Updates the graph object using a vector of decision variables
+% This canbe used to display a solved graph
 function Tcopy = update_graph(T, x)
     Tcopy = T;
     for i=1:height(Tcopy.Nodes)
@@ -45,10 +56,7 @@ function Tcopy = update_graph(T, x)
     end
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
+% Gives the constraints of a graph in a form usable by fmincon
 function [c, ceq] = cons(x, T, b)
     % Inequality constraints. Edge lengths no greater than b
     c = [];
@@ -69,6 +77,7 @@ function [c, ceq] = cons(x, T, b)
     end
 end
 
+% Gives the objective function in a form usable by fmincon
 function obj = f(x, T)
     obj = 0;
     for k=1:height(T.Edges)
@@ -78,10 +87,9 @@ function obj = f(x, T)
     end
 end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+% Calculates the length of every edge.
+% In an old version of the graph model, these edge lengths were saved into
+% the graph object as the edge weights, hence the name.
 function weights = calculate_weights_at_x(T, x)
     Tcopy = T;
     for i=1:height(Tcopy.Nodes)
@@ -100,6 +108,7 @@ function weights = calculate_weights_at_x(T, x)
     end
 end
 
+% Gradient of the objective function.
 function gradf = gradf(T, x)
     update_graph(T, x);
     gradf = zeros([height(x) 1]);
@@ -115,6 +124,7 @@ function gradf = gradf(T, x)
     end
 end
 
+% Returns an array g of the inequality constraints g_i
 function g = g(x, T, b)
     T = update_graph(T, x);
     g = [];
@@ -126,6 +136,7 @@ function g = g(x, T, b)
 end
 
 % Gradient of inequality constraints
+% Returns an array of column vectors
 function gradg = gradg(x, T)
     T = update_graph(T, x);
     gradg = [];
@@ -142,8 +153,8 @@ function gradg = gradg(x, T)
 end
 
 % Equality constraints
+% Returns an array of values
 function h = h(x, T)
-    %T = update_graph(T, x);
     h = [];
     for i=1:height(T.Nodes)
       if T.Nodes.Steiner{i}==0
@@ -154,8 +165,8 @@ function h = h(x, T)
 end
 
 % Gradient of equality constraints
+% Returns an array of column vectors
 function gradh = gradh(x, T)
-    %T = update_graph(T, x);
     gradh = [];
     for i=1:height(T.Nodes)
         if T.Nodes.Steiner{i}==0
@@ -168,6 +179,8 @@ function gradh = gradh(x, T)
     end
 end
 
+% Produces the penalty function and its gradient for a given graph T, a
+% given edge length restriction b, and a given penalty value k.
 function [p, gradp] = P(x, T, b, k)
     c = g(x, T, b);
     ceq = h(x, T);
@@ -180,6 +193,8 @@ function [p, gradp] = P(x, T, b, k)
     gradp = gradf(T, x) - (gradc * (1./(k.*c)).') + (gradceq * k*ceq.');
 end
 
+% These functions help produce the function handles for the penalty
+% functions. These are used as input into the steepest descent solver.
 function out = P_p(x, Tpenalty, b, k)
     [out,~] = P(x,Tpenalty,b,k);
 end
@@ -187,13 +202,7 @@ function out = P_grad(x, Tpenalty, b, k)
     [~,out] = P(x,Tpenalty,b,k);
 end
 
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-%
-
-
+% Generates a random instance of a graph with n terminal points
 function T = generate_graph(n)
     A = [0 1; 1 0];
     NodeTable = table({0;0}, {0;1}, {0;0}, VariableNames={'x', 'y', 'Steiner'});
@@ -203,13 +212,12 @@ function T = generate_graph(n)
     end
 end
 
+% Solves the problem using fmincon, MATLAB's built in NLP solver
 function xminEstimate = matlab_solver(T, b)
-
     % Create a conditions function with the treet T and edge length b built in
     specific_cons = @(x)cons(x, T, b);
 
     % Define the objective function
-    
     objfun = @(x)f(x, T);
 
     x0 = [];
@@ -221,6 +229,7 @@ function xminEstimate = matlab_solver(T, b)
     xminEstimate = fmincon(objfun, x0, [], [], [], [], [], [], specific_cons);
 end
 
+% Solves the problem using a custom log-barrier penalty method.
 function xminEstimate = penalty_method(T, b, k, tolerance1, tolerance2, t)
     x0 = [];
     for i=1:height(T.Nodes)
@@ -233,10 +242,10 @@ function xminEstimate = penalty_method(T, b, k, tolerance1, tolerance2, t)
     [xminEstimate, fminEstimate,k] = steepestDescentMethod(p, gradp, x0, tolerance1, tolerance2, t);
 end
 
+% Experimental setup used to compare fmincon with our own NLP solver implementation
 m = [];
 p = [];
 ns = [3 4 5 7 10 15];
-%ns = [3 4];
 
 for n=ns
     mtotal = 0;
